b6a4b92f3c420477081c0e0165cc3987f99d9667
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2024-04-21 15:34:50 +0200

    edit ci

--- test.js
+++ test.js
@@ -1,12 +1,5 @@
 
-test('CI/CD pipeline flow test (always passes)', () => {
-    
-    console.log('Simulating code changes...');
-  
-    console.log('Building the application...');
-    console.log('Running tests...');
-    console.log('Deploying to staging...');
-  
-    expect(true).toBe(true); 
-  });
-  
+test('CI/CD pipeline flow test', () => {
+
+    expect(true).toBe(true);
+});
